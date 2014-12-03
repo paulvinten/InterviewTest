5bdd9a699a28cf7f428fee64a3f09940d578af86
Paul Vinten <cdex@paulvinten.co.uk>
2014-12-03 14:38:45 +0000

    Initial tests

---
+++ FileComparisonHandle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundsLibraryTest
+{
+    class FileComparisonHandle
+    {
+        private FileInfo m_fileInfo;
+
+        public FileComparisonHandle(string fileName)
+        {
+            // TODO: Complete member initialization
+            this.m_fileInfo = new FileInfo(fileName);
+
+            if (!m_fileInfo.Exists)
+            {
+                throw new FileNotFoundException();
+            }
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundsLibraryTest
+{
+    [TestClass]
+    class Tests
+    {
+        [TestMethod]
+        public void CheckEqualBinsAreEqual ()
+        {
+            var handle1 = new FileComparisonHandle("file_0.bin");
+            var handle2 = new FileComparisonHandle("file_1.bin");
+        }
+
+        [TestMethod]
+        public void CheckNonExistantFile()
+        {
+            try
+            {
+                var handle1 = new FileComparisonHandle("file_0.bin");
+
+                Assert.Fail();
+
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(FileNotFoundException));
+            }
+            
+        }
+    }
+}
